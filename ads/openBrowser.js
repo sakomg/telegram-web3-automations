c71bd862f160d8a8cd8968b7681c72a84a2ccf30
Alexander Komegunov <a_komegunov@twistellar.com>
2024-06-19 13:10:43 +0300

    debug

--- ads/openBrowser.js
+++ ads/openBrowser.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 import logger from '../logger/logger.js';
 
 const adsOpenBrowser = async (userId) => {
+  logger.info('adsOpenBrowser');
   try {
     const { data } = await axios(`http://local.adspower.net:50325/api/v1/browser/start?user_id=${userId}`);
+    logger.info('adsOpenBrowser');
+    console.log(data);
     return data;
   } catch (e) {
     logger.error(e);
